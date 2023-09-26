d368a9ddfe72f5d14d4a3fe6c97c7d8c8f1406dc
lubrian0383 <chukwumaegbujor@gmail.com>
2023-09-26 11:35:24 +0100

    myniitclasswork

---
+++ iindex.js
@@ -0,0 +1,119 @@
+// js keywords
+
+// let const = "man"
+// let else = 3
+// let if = null
+// let break = hello
+// let function = hello
+// let defaults = hello
+// let return = "hello"
+// let do = "hello"
+
+
+
+// strings methods
+
+let firstname = "favour"
+console.log(firstname);
+//toUppercase
+let class_Name = "prosper"
+console.log(class_Name.toLowerCase())
+
+//length
+console.log(class_Name.indexOf("k"));
+
+//indexof
+console.log(class_Name.indexOf("k"));
+
+//charAT
+let neWName = "nonso"
+
+console.log(neWName.charAt())
+
+//splice
+let recordName = "freedom"
+console.log(recordName.slice(4,7));
+
+//splice
+
+
+
+
+
+//math method / Number Methods
+
+let number = 99
+console.log(number);
+let numberResult = number.toString()
+console.log(numberResult);
+
+
+//Math Method
+const maths = Math.sqrt(16)
+console.log(maths);
+let results =  Math.cos(45)
+console.log(results);
+
+let anotherNumber = Math.pow (4,2)
+console.log = (anotherNumber);
+
+let newNumbers = Math.random()
+// console.log = (newNumber);
+
+// let newNumber = math.random()*10
+// console.log(newNumber);
+// let newNumber = (math.floor(math.random()*100)
+// let newNumber = (math.ceil (math.random()*100)
+// console.log(newNumber,ceilNumber);
+// let roundNumber = math.round(math.random()*100)
+
+// method1
+//let word = "how are you doing today";
+//let wordBefore = word.length;
+//let wordReplace = word.replace(/\s/g," ");
+//let wordAfter = word.replace.length;
+//console.log (word);
+//console.log (wordBefore);
+//console.log (wordReplace);
+//console.log (wordAfter);
+//method2
+// let text="Hell no"
+//console.log(text);
+//let textBeforeLength = text.replaceAll(" ", "")
+//console.log(replacedText);
+//let replaceTextLength = replacedText.length;
+//console.log(replacedTextLength);
+
+//Ans to question2
+//let Best_mc = "Eminem and no one else";
+//console.log(Best_mc);
+//let wack_mc = ("no one else, shaddy");
+//console.log(wack_mc);
+
+const names = "bad man,Good man,Yellow boy";
+const resultName = names.split("");
+console.log(resultName);
+
+//push
+const newArr = ["Finbarr",34,true];
+const arrResult = newArr.push("Money")
+console.log("ArrayPopped");
+console.log(arrayPop);
+
+//unshift
+let unshiftArr = [1,3,5,7];
+let unshiftResult = unshiftArr.unshift(0);
+console.log(unshiftResult);
+console.log(unshiftArr);
+
+//shift
+let shiftArr = [4,6,8];
+let shiftArrResult = unshiftArr.shift();
+console.log(shiftArrResult);
+console.log(shiftArr);
+
+//reverse
+let reverse = [1,2,3,4,5,6,7];
+let reverseArr = names.reverse();
+console.log(reversedArr);
+
